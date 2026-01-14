9bb4c56bd6c452b4e3e40ad72cb6b40af7b102fd
Fabián Vargas <fabidev18@gmail.com>
2026-01-13 23:51:17 -0600

    fix: update document title in root route and add head metadata to settings route

--- src/routes/_app/settings.tsx
+++ src/routes/_app/settings.tsx
@@ -5,6 +5,13 @@ import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_app/settings')({
   component: RouteComponent,
+  head: () => ({
+    meta: [
+      {
+        title: 'Settings'
+      }
+    ]
+  })
 })
 
 function RouteComponent() {
